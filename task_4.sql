150c982a980ca45143a0b2f1131597325c479df6
Oussama <oussamay713@gmail.com>
2024-10-19 20:40:39 +0200

    task 4 updated

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,3 @@
-USE alx_book_store;
-
 SELECT 
     COLUMN_NAME, 
     COLUMN_TYPE, 
@@ -11,4 +9,4 @@ FROM
     INFORMATION_SCHEMA.COLUMNS 
 WHERE 
     TABLE_NAME = 'Books' 
-    AND TABLE_SCHEMA = DATABASE();
+    AND TABLE_SCHEMA = 'alx_book_store';
